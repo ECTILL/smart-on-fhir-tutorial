2f8d08537c8b0a8eabe86ca2550c846c980375e4
ECTILL <ethan.watson@cerner.com>
2016-12-27 18:39:19 +0000

    publish: Console patient
    generated from commit fc0590f15218949c1fe85cdba5d6066ce5099ecb

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -25,7 +25,7 @@
         $.when(pt, obv).fail(onError);
 
         $.when(pt, obv).done(function(patient, obv) {
-          // console.dir(patient)
+          console.dir(patient)
           var byCodes = smart.byCodes(obv, 'code');
           var gender = patient.gender;
           var dob = new Date(patient.birthDate);
